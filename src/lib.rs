77060b7dc6d548832fc8e9bd4e9a6063dda426e9
Jack O'Connor <oconnor663@gmail.com>
2016-03-21 00:06:53 -0400

    define the .read() method to some approximation

--- src/lib.rs
+++ src/lib.rs
@@ -4,21 +4,49 @@ use std::ffi::{OsStr, OsString};
 use std::fmt::Debug;
 use std::fs::File;
 use std::io;
+use std::io::prelude::*;
 use std::mem;
 use std::os::unix::io::{FromRawFd, IntoRawFd, RawFd};
 use std::path::{Path, PathBuf};
-use std::process::{Command, Stdio, Output};
+use std::process::{Command, Stdio, Output, ExitStatus};
+use std::thread::JoinHandle;
 
 pub trait Expression: Clone + Debug {
-    fn exec(&self, context: IoContext) -> Result;
+    fn exec(&self, context: IoContext) -> io::Result<ExitStatus>;
 
-    fn run(&self) -> Result {
+    fn run(&self) -> Result<Output, Error> {
         let context = IoContext {
             stdin: CloneableStdio::Inherit,
             stdout: CloneableStdio::Inherit,
             stderr: CloneableStdio::Inherit,
         };
-        self.exec(context)
+        let status = try!(self.exec(context));
+        Ok(Output{
+            status: status,
+            stdout: Vec::new(),
+            stderr: Vec::new(),
+        })
+    }
+
+    fn read(&self) -> Result<String, Error> {
+        let (stdout, stdout_reader) = pipe_with_reader_thread();
+        let context = IoContext {
+            stdin: CloneableStdio::Inherit,
+            stdout: CloneableStdio::Fd(stdout),
+            stderr: CloneableStdio::Inherit,
+        };
+        let status = try!(self.exec(context));
+        let output = Output{
+            status: status,
+            stdout: try!(stdout_reader.join().unwrap()),
+            stderr: Vec::new(),
+        };
+        if output.status.success() {
+            // TODO: should only trim newlines
+            Ok(try!(String::from_utf8(output.stdout)).trim_right().to_string())
+        } else {
+            Err(Error::Status(output))
+        }
     }
 }
 
@@ -48,7 +76,7 @@ impl ArgvCommand {
 }
 
 impl Expression for ArgvCommand {
-    fn exec(&self, context: IoContext) -> Result {
+    fn exec(&self, context: IoContext) -> io::Result<ExitStatus> {
         let IoContext{stdin, stdout, stderr} = context;
         // Create a Command and add the args.
         let mut command = Command::new(&self.argv[0]);
@@ -62,12 +90,7 @@ impl Expression for ArgvCommand {
                 FromRawFd::from_raw_fd(file.into_raw_fd())
             });
         }
-        let exit_status = try!(command.status());
-        Ok(Output{
-            stdout: Vec::new(),
-            stderr: Vec::new(),
-            status: exit_status,
-        })
+        Ok(try!(command.status()))
     }
 }
 
@@ -85,7 +108,7 @@ impl Pipe {
 }
 
 impl Expression for Pipe {
-    fn exec(&self, context: IoContext) -> Result {
+    fn exec(&self, context: IoContext) -> io::Result<ExitStatus> {
         let IoContext{stdin, stdout, stderr} = context;
         let (read_pipe, write_pipe) = open_pipe();
         let left_context = IoContext{
@@ -102,26 +125,31 @@ impl Expression for Pipe {
         let left_thread = std::thread::spawn(move || {
             left_clone.exec(left_context)
         });
-        let right_result = self.right.exec(right_context);
-        let left_result = left_thread.join().unwrap();  // TODO: handle errors here?
-        match right_result {
-            Err(_) => right_result,
-            _ => left_result,
+        let right_status = self.right.exec(right_context);
+        let left_status = left_thread.join().unwrap();  // TODO: handle errors here?
+        match right_status {
+            Err(_) => right_status,
+            _ => left_status,
         }
     }
 }
 
-pub type Result = std::result::Result<Output, Error>;
-
 #[derive(Debug)]
 pub enum Error {
     Io(io::Error),
+    Utf8(std::string::FromUtf8Error),
     Status(Output),
 }
 
 impl From<io::Error> for Error {
     fn from(err: io::Error) -> Error {
         Error::Io(err)
+    }
+}
+
+impl From<std::string::FromUtf8Error> for Error {
+    fn from(err: std::string::FromUtf8Error) -> Error {
+        Error::Utf8(err)
     }
 }
 
@@ -200,6 +228,17 @@ fn open_pipe() -> (CloneableFd, CloneableFd) {
     }
 }
 
+fn pipe_with_reader_thread() -> (CloneableFd, JoinHandle<io::Result<Vec<u8>>>) {
+    let (read_pipe, write_pipe) = open_pipe();
+    let thread = std::thread::spawn(move || {
+        let mut read_file = unsafe { File::from_raw_fd(read_pipe.into_raw_fd()) };
+        let mut output = Vec::new();
+        try!(read_file.read_to_end(&mut output));
+        Ok(output)
+    });
+    (write_pipe, thread)
+}
+
 #[cfg(test)]
 mod test {
     use super::{ArgvCommand, Pipe, Expression};
@@ -224,6 +263,12 @@ mod test {
     }
 
     #[test]
+    fn test_read() {
+        let output = ArgvCommand::new("echo").arg("hi").read().unwrap();
+        assert_eq!(output, "hi");
+    }
+
+    #[test]
     fn test_stdout() {
         let path = mktemp();
         let result = ArgvCommand::new("echo").arg("hi").stdout(&path).run();
@@ -239,13 +284,8 @@ mod test {
         left.arg("hi");
         let mut right = ArgvCommand::new("sed");
         right.arg("s/i/o/");
-        let path = mktemp();
-        right.stdout(&path);
         let pipe = Pipe::new(&left, &right);
-        let result = pipe.run();
-        assert!(result.unwrap().status.success());
-        let mut contents = String::new();
-        File::open(&path).unwrap().read_to_string(&mut contents).unwrap();
-        assert_eq!(contents, "ho\n");
+        let output = pipe.read().unwrap();
+        assert_eq!(output, "ho");
     }
 }
